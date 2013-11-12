778a24a8b3f27638941baf6a9c6e9bb4e0f37e3b
Bjoern Schneider <bsc71@gmx.de>
2013-11-12 18:23:58 +0100

    Lock down specific projects controller actions for admins only

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,11 @@ module ApplicationHelper
   def title(*parts)
     unless parts.empty?
       content_for :title do
-      (parts << "Ticketee").join(" - ")
+        (parts << "Ticketee").join(" - ")
       end
     end
+  end
+  def admins_only(&block)
+    block.call if current_user.try(:admin?) # try if current_user is set AND admin=true
   end
 end
