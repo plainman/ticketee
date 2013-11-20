9a161de699af0ad5269366f67c65655d8c03f384
Bjoern Schneider <bsc71@gmx.de>
2013-11-20 08:11:08 +0100

    Uploaded Assets are now strictly served through the FilesController

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Ticketee::Application.routes.draw do
 
   delete "/signout", to: "sessions#destroy", as: "signout"
 
+  resources :files
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
---
+++ startall.bat
@@ -0,0 +1,5 @@
+start rails console
+start rails server
+start sqlite3 db\development.sqlite3
+rem "C:\Program Files\Sublime Text 3\sublime_text.exe"
+rem "C:\Users\Default.DEV-Lap-011\Dropbox\Eigene Dateien\Artikel, Vorträge, Bücher\SW-Entwicklung\Ruby (on Rails)\Rails_4_in_Action_v11_MEAP (selber gekauft!).pdf"
